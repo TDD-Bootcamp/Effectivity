a29c1cd674d7e7c42eeab810527c8e25e0ff5236
Vic Wu <madcocomo@gmail.com>
2018-01-30 23:26:40 +0800

    support mutiple ranges

--- src/main/java/tdd/course/Effectivity.java
+++ src/main/java/tdd/course/Effectivity.java
@@ -1,6 +1,7 @@
 package tdd.course;
 
 public class Effectivity {
+    public static final String ALL_EFFECT = "ALL";
     private String ranges;
 
     public Effectivity(String ranges) {
@@ -8,7 +9,22 @@ public class Effectivity {
     }
 
     public boolean isEffect(String tail) {
-        if (ranges.equals("ALL")) {
+        if (ranges.equals(ALL_EFFECT)) {
+            return true;
+        }
+        String[] rangeArr = ranges.split(", ");
+        boolean isEffect = false;
+        for (String range : rangeArr) {
+            isEffect |= isInRange(tail, range);
+        }
+        return isEffect;
+    }
+
+    private boolean isInRange(String tail, String ranges) {
+        String[] limits = ranges.split("-");
+        String lowLimit = limits[0];
+        String highLimit = limits[1];
+        if (tail.compareTo(lowLimit) >= 0 && tail.compareTo(highLimit) <= 0) {
             return true;
         }
         return false;
--- src/test/java/tdd/course/EffectivityTest.java
+++ src/test/java/tdd/course/EffectivityTest.java
@@ -17,4 +17,34 @@ public class EffectivityTest {
         Effectivity effectivity = new Effectivity("200-300");
         assertThat(effectivity.isEffect("123"), is(false));
     }
+
+    @Test
+    public void should_take_effect_if_tail_inside_range() {
+        Effectivity effectivity = new Effectivity("200-300");
+        assertThat(effectivity.isEffect("223"), is(true));
+    }
+
+    @Test
+    public void should_take_effect_if_tail_equal_to_range_begin() {
+        Effectivity effectivity = new Effectivity("200-300");
+        assertThat(effectivity.isEffect("200"), is(true));
+    }
+
+    @Test
+    public void should_not_take_effect_if_tail_higher_than_range_end() {
+        Effectivity effectivity = new Effectivity("200-300");
+        assertThat(effectivity.isEffect("323"), is(false));
+    }
+
+    @Test
+    public void should_take_effect_if_tail_equals_to_range_end() {
+        Effectivity effectivity = new Effectivity("200-300");
+        assertThat(effectivity.isEffect("300"), is(true));
+    }
+
+    @Test
+    public void should_take_effect_if_tail_inside_2nd_range() {
+        Effectivity effectivity = new Effectivity("200-300, 320-400");
+        assertThat(effectivity.isEffect("323"), is(true));
+    }
 }
